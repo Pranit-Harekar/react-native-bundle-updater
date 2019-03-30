6ced650ab7f2328f9f540a56c4356753c1ba4577
Pranit-Harekar <pranitharekar@hotmail.com>
2019-03-30 17:52:07 -0400

    Basic React Native setup

---
+++ ios/ReactNativeBundleUpdater/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ReactNativeBundleUpdater
+//
+//  Created by Pranit  Harekar on 3/30/19.
+//  Copyright © 2019 Pranit Harekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
